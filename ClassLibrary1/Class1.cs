eff2673a3676a5bbbcef9b0a90ed4634a29356f0
eshhusv <ak7527203@gmail.com>
2023-03-04 23:20:23 +0200

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace ClassLibrary1
+
+{
+
+    public class Class1
+
+    {
+
+        public static int Name(int[,] arr)
+
+        {
+
+            int res = 0;
+
+            int minSum = 2147483647;
+
+            for (int j = 0; j < arr.GetLength(1); j++)
+
+            {
+
+                int sum = 0;
+
+                for (int i = 0; i < arr.GetLength(0); i++)
+
+                {
+
+                    sum += arr[i, j];
+
+                }
+
+                if (minSum > sum) { minSum = sum; res = j; }
+
+            }
+
+            return res;
+
+        }
+
+    }
+
+}
---
+++ sixdottwo/Program.cs
@@ -0,0 +1,40 @@
+﻿using ClassLibrary1;
+using System;
+
+int[,] a = new int[5, 8];
+
+for (int i = 0; i < a.GetLength(0); i++)
+
+{
+
+    for (int j = 0; j < a.GetLength(1); j++)
+
+    {
+
+        Console.Write($"a[{i},{j}]: ");
+
+        a[i, j] = int.Parse(Console.ReadLine());
+
+    }
+
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < a.GetLength(0); i++)
+
+{
+
+    for (int j = 0; j < a.GetLength(1); j++)
+
+    {
+
+        Console.Write($"\t{a[i, j]}");
+
+    }
+
+    Console.WriteLine();
+
+}
+
+Console.WriteLine($"\nСтолбец с минимальной суммой элементов: {Class1.Name(a) + 1}");
